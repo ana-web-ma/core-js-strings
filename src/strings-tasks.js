83f6d3b3977842416ee9c720bcc99b12f46fb44b
ana-web-ma <ana.web.ma@gmail.com>
2025-09-16 23:00:08 +0300

    feat: add solutions 12/43

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -37,8 +37,14 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return (
+    typeof value === 'string' ||
+    (value &&
+      typeof value === 'object' &&
+      typeof value.valueOf() === 'string') ||
+    false
+  );
 }
 
 /**
